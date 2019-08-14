5132d89381e51504ba7dc4adb147e4eae7be4c03
royderks <10717410+royderks@users.noreply.github.com>
2019-08-14 17:22:42 +0200

    Created all the needed functions to setup the matomo tracker for tracking events and pageviews

--- src/MatomoTracker.js
+++ src/MatomoTracker.js
@@ -1,76 +1,110 @@
 const defaultOptions = {
   urlBase: null,
-  siteId: 1, // production
-}
+  siteId: 1
+};
+
+const TRACK_TYPES = {
+  TRACK_EVENT: "trackEvent",
+  TRACK_SEARCH: "trackSiteSearch",
+  TRACK_VIEW: "trackPageView"
+};
 
 class MatomoTracker {
-  static TRACK_TYPES = {
-    TRACK_EVENT: 'trackEvent',
-    TRACK_SEARCH: 'trackSiteSearch',
-    TRACK_VIEW: 'trackPageView',
-  }
-
   constructor(userOptions) {
-    const options = { ...defaultOptions, ...userOptions }
+    const options = { ...defaultOptions, ...userOptions };
     if (!options.urlBase) {
-      new Error('Matomo urlBase is not set.')
+      new Error("Matomo urlBase is not set.");
     }
 
-    this.initialize(options)
+    this.initialize(options);
   }
 
-  initialize({ urlBase, siteId }) {
-    window._paq = window._paq || []
+  initialize({ urlBase, siteId, trackerUrl, srcUrl }) {
+    window._paq = window._paq || [];
 
     if (window._paq.length === 0) {
-      window._paq.push(['enableLinkTracking'])
-      window._paq.push(['setTrackerUrl', `${urlBase}piwik.php`])
-      window._paq.push(['setSiteId', siteId])
+      window._paq.push(["enableLinkTracking"]);
+      window._paq.push([
+        "setTrackerUrl",
+        trackerUrl ? trackerUrl : `${urlBase}matomo.php`
+      ]);
+      window._paq.push(["setSiteId", siteId]);
 
-      const doc = document
-      const scriptElement = doc.createElement('script')
-      const scripts = doc.getElementsByTagName('script')[0]
+      const doc = document;
+      const scriptElement = doc.createElement("script");
+      const scripts = doc.getElementsByTagName("script")[0];
 
-      scriptElement.type = 'text/javascript'
-      scriptElement.async = true
-      scriptElement.defer = true
-      scriptElement.src = `${urlBase}piwik.js`
+      scriptElement.type = "text/javascript";
+      scriptElement.async = true;
+      scriptElement.defer = true;
+      scriptElement.src = srcUrl ? srcUrl : `${urlBase}matomo.js`;
 
-      scripts.parentNode.insertBefore(scriptElement, scripts)
+      scripts.parentNode.insertBefore(scriptElement, scripts);
     }
   }
 
   trackEvents() {
-    const clickEvents = [...document.querySelectorAll('[data-matomo-event="click"]')]
-    clickEvents.forEach((element) => {
-      element.addEventListener('click', () => {
-        const { matomoCategory, matomoQuery } = element.dataset
-        this.track([matomoCategory, matomoQuery])
-      })
-    })
+    const clickEvents = [
+      ...document.querySelectorAll('[data-matomo-event="click"]')
+    ];
+
+    clickEvents.forEach(element => {
+      element.addEventListener("click", () => {
+        const { matomoAction, matomoName, matomoValue } = element.dataset;
+        this.trackEvent({
+          action: matomoAction,
+          name: matomoName,
+          value: matomoValue
+        });
+      });
+    });
   }
 
-  trackPageView() {
-    this.track()
+  trackEvent({
+    action,
+    name,
+    value,
+    documentTitle = window.document.title,
+    href = window.location,
+    customDimensions = false
+  }) {
+    if (!action) {
+      throw new Error(`Error: action is not defined.`);
+    }
+    this.track(
+      [TRACK_TYPES.TRACK_EVENT, action, name, value],
+      documentTitle,
+      href,
+      customDimensions
+    );
   }
 
-  track(data = [], customDimensions = false) {
-    const href = window.location.href
-    const title = window.document.title
+  trackPageView({
+    documentTitle = window.document.title,
+    href = window.location,
+    customDimensions = false
+  }) {
+    this.track([TRACK_TYPES.TRACK_VIEW], documentTitle, href, customDimensions);
+  }
 
+  track(data = [], documentTitle, href, customDimensions = false) {
     if (data.length) {
       if (customDimensions.length) {
-        customDimensions.map(
-          (customDimension) => window._paq.push(['setCustomDimension', customDimension.id, customDimension.value]),
-        )
+        customDimensions.map(customDimension =>
+          window._paq.push([
+            "setCustomDimension",
+            customDimension.id,
+            customDimension.value
+          ])
+        );
       }
 
-      window._paq.push(['setCustomUrl', href])
-      window._paq.push(['setDocumentTitle', title])
-      window._paq.push(['enableHeartBeatTimer']) // accurately measure the time spent on the last pageview of a visit
-      window._paq.push(data)
+      window._paq.push(["setCustomUrl", href]);
+      window._paq.push(["setDocumentTitle", documentTitle]);
+      window._paq.push(["enableHeartBeatTimer"]); // accurately measure the time spent on the last pageview of a visit
+      window._paq.push(data);
     }
   }
 }
 
-export default MatomoTracker
+export default MatomoTracker;
