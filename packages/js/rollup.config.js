68bf4e7c1e54e44b6894e8715329fe5f0b53d9cc
royderks <10717410+royderks@users.noreply.github.com>
2019-08-19 15:42:26 +0200

    Change repo to a monorepo

---
+++ packages/js/rollup.config.js
@@ -0,0 +1,11 @@
+import typescript from "rollup-plugin-typescript";
+import { uglify } from "rollup-plugin-uglify";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "bundle.min.js",
+    format: "iife"
+  },
+  plugins: [typescript({ target: "es5", declarationMap: false }), uglify()]
+};
