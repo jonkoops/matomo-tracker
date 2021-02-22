b28e2ad85ae5d8f6077922fc498919074c444a24
Jon Koops <jonkoops@gmail.com>
2021-02-22 13:47:46 +0100

    fix linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:prettier/recommended',
     'plugin:jest/recommended',
-    'prettier/@typescript-eslint',
   ],
   parserOptions: {
     project: './tsconfig.eslint.json',
