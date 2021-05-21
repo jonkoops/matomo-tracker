34618c7a73e2ec3f8acbe9a1b33d4f049665c9d7
Michael S <9484406+codebymikey@users.noreply.github.com>
2021-05-21 16:26:26 +0100

    Update useOutboundClickListener.tsx
    Add support for the onclick event of AnchorElements child elements.

--- packages/react/src/utils/useOutboundClickListener.tsx
+++ packages/react/src/utils/useOutboundClickListener.tsx
@@ -3,7 +3,18 @@ import { MatomoInstance } from '../types'
 
 const useOutboundClickListener = (matomoInstance: MatomoInstance): void => {
   const handleOutboundClick = (event: MouseEvent) => {
-    const { target } = event
+    // The target is not guaranteed to be a link, it could be a child element.
+    // Look up the element's parent until the anchor element is found.
+    const findLinkElement = (el) => {
+      if (el instanceof HTMLAnchorElement && el.href) {
+        return el
+      } else if (el && el.parentElement) {
+        return findLinkElement(el.parentElement)
+      }
+      return null
+    }
+
+    const target = findLinkElement(event.target)
 
     if (!(target instanceof HTMLAnchorElement)) {
       return
