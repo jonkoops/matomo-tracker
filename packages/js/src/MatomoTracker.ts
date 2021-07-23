38c7bb50698c3f92fc9c06133486e7286d6cbf00
SassNinja <kai.falkowski@gmail.com>
2021-07-23 15:27:40 +0200

    Move disableAlwaysUseSendBeacon call
    This can't be called befire matomo is loaded.

--- packages/js/src/MatomoTracker.ts
+++ packages/js/src/MatomoTracker.ts
@@ -83,12 +83,6 @@ class MatomoTracker {
     // // might not work accurately on SPAs because new links (dom elements) are created dynamically without a server-side page reload.
     this.enableLinkTracking(linkTracking)
 
-    // If enabled (default), always use sendBeacon if the browser supports it
-    // Disabling this makes sense when you're waiting for a tracking operation to be done and immediately do a page redirect afterwards.
-    if (alwaysUseSendBeacon === false) {
-      this.callMethod('disableAlwaysUseSendBeacon')
-    }
-
     const doc = document
     const scriptElement = doc.createElement('script')
     const scripts = doc.getElementsByTagName('script')[0]
@@ -99,6 +93,12 @@ class MatomoTracker {
     scriptElement.src = srcUrl || `${normalizedUrlBase}matomo.js`
     scriptElement.onload = () => {
       this.asyncTrackers = window.Matomo.getAsyncTrackers()
+
+      // If enabled (default), always use sendBeacon if the browser supports it
+      // Disabling this makes sense when you're waiting for a tracking operation to be done and immediately do a page redirect afterwards.
+      if (alwaysUseSendBeacon === false) {
+        this.callMethod('disableAlwaysUseSendBeacon')
+      }
     }
 
     if (scripts && scripts.parentNode) {
