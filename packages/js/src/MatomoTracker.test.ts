69ab653b337b2cf8ba55d6db362e1b1d04c43cbd
Chris van Mook <chrisvanmook@gmail.com>
2021-08-06 11:47:46 +0200

    Make configurations parameter support array of instructions

--- packages/js/src/MatomoTracker.test.ts
+++ packages/js/src/MatomoTracker.test.ts
@@ -4,6 +4,24 @@ import { UserOptions } from './types'
 const URL_BASE = 'https://example.com'
 
 describe('MatomoTracker', () => {
+  it('should build the window._paq correctly on initialisation', () => {
+    window._paq = []
+    // eslint-disable-next-line no-new
+    new MatomoTracker({
+      siteId: 1,
+      urlBase: 'https://foo.bar',
+      configurations: { setCustomDimension: [1, 'someValue'], foo: 'bar' },
+    })
+    expect(window._paq).toEqual([
+      ['setTrackerUrl', 'https://foo.bar/matomo.php'],
+      ['setSiteId', 1],
+      ['setCustomDimension', 1, 'someValue'],
+      ['foo', 'bar'],
+      ['enableHeartBeatTimer', 15],
+      ['enableLinkTracking', true],
+    ])
+  })
+
   it('throws an error if no urlBase is passed in options', () => {
     expect(() => new MatomoTracker({ siteId: 1 } as UserOptions)).toThrowError()
   })
