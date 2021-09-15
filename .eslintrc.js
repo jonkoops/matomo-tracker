0d119a767a41146168a2d6efac246fe8ba5f3d81
Jon Koops <jonkoops@gmail.com>
2021-09-15 10:18:20 +0200

    Merge pull request #621 from Amsterdam/feature/update-eslint-deps
    Update ESLint dependencies to latest version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: [
+    'airbnb',
     'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
