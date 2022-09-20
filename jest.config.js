aca4ef549292f2ca07e6e5437fde4b17906230e4
Jon Koops <jonkoops@gmail.com>
2022-09-20 13:37:24 +0200

    Update Jest to version 29 (#941)

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   testEnvironment: 'jsdom',
+  preset: 'ts-jest',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   moduleDirectories: ['node_modules', 'src'],
   testPathIgnorePatterns: ['/node_modules/', '/lib/', '/es/', '/examples/'],
   transform: {
-    '\\.(ts|tsx)$': 'ts-jest',
-  },
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.json',
-    },
+    '\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
   },
   testRegex: ['/__tests__/.*\\.(ts|tsx|js)$', '/*.test\\.(ts|tsx|js)$'],
 }
